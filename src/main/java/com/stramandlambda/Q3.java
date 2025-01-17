99cc8add079fc2e14aeccd8fb686a03b0e72e956
Imalsha <imalsha.kavindi96@gmail.com>
2025-01-17 11:27:51 +0530

    Created Q3.java

---
+++ src/main/java/com/stramandlambda/Q3.java
@@ -0,0 +1,15 @@
+package com.stramandlambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q3 {
+    public static void main(String[] args) {
+        List<String> nameList = Arrays.asList("Kalana","Kaushalya","Chathurika");
+        System.out.println(nameList);
+       List<String> newList = nameList.stream().map(String::toLowerCase).toList();
+        System.out.println(nameList);
+        System.out.println(nameList.stream().map(String::toUpperCase).toList());
+    }
+}
