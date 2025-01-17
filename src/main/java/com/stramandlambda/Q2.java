f42d42326de16da22584f4ac62714f7bda763f77
Imalsha <imalsha.kavindi96@gmail.com>
2025-01-17 11:04:09 +0530

    Stream API and Lambda added

---
+++ src/main/java/com/stramandlambda/Q2.java
@@ -0,0 +1,14 @@
+package com.stramandlambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q2 {
+    public static void main(String[] args) {
+
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    List<Integer> evenList = list.stream().filter(x -> x % 2 == 0).toList();
+
+    System.out.println(evenList);
+    }
+}
